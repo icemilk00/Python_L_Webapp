455ffc06976be527bf65b6a927a0652bf3ac320e
icemilk00 <442377973@qq.com>
2015-11-23 20:04:26 +0800

    add note for orm.py

--- www/orm.py
+++ www/orm.py
@@ -28,10 +28,9 @@ def create_pool(loop, **kw):
 #select函数，负责查询
 @asyncio.coroutine
 def select(sql, args, size=None):
-	log(sql, args)
 	global __pool
 	#从连接池取一个conn出来，with..as..会在运行完后把conn放回连接池
-	with (yield from _pool) as conn:
+	with (yield from __pool) as conn:
 		#获取一个cursor，通过aiomysql.DictCursor获取到的cursor在返回结果时会返回一个字典格式
 		cur = yield from conn.cursor(aiomysql.DictCursor)
 		#把sql语句的'?'替换为'%s',并把args的值填充到相应的位置补充成完整的可执行sql语句并执行，mysql中得占位符是'?'，python中得为'%s'
@@ -123,59 +122,66 @@ class ModelMetalclass(type):
 			return type.__new__(cls, name, bases, attrs)
 
 		#Model的子类会继续往下走
-		#获取table名称：
+		#获取table名称：attrs的'__table__'键对应的value，如果为空的话则用name字段的值
 		tableName = attrs.get('__table__', None) or name
 		logging.info('found model : %s (table: %s)' % (name, tableName))
 		#获取所有的Field和主键名
-		mappings = dict()
-		fields = []
-		primaryKey = None
-		for k, v in attrs.items():
-			if isinstance(v, Field):
+		mappings = dict()	#mappings字典，存放所有Field键值对，属性名：value
+		fields = []			#fields数组，存放除了主键以外的属性名
+		primaryKey = None	#primaryKey主键
+		for k, v in attrs.items():			#k,v 分别对应创建时传进来的需要赋值的属性名，和要赋的值
+			if isinstance(v, Field):		#查看value是不是Field类型，是的话继续
 				logging.info('found mapping : %s ==> %s' % (k, v))
-				mappings[k] = v
-				if v .primary_key:
+				mappings[k] = v 			#把符合要求的放到mappings里
+				if v.primary_key:			#如果当前Field是主键，则记录下来
 					#找到主键
 					if primaryKey:
 						raise RuntimeError('Duplicate primary key for field:%s' % k)
-					primaryKey = k
+					primaryKey = k 			#记录主键
 				else:
-					fields.append(k)
+					fields.append(k)		#不是主键的话把key值放到fields里
 
 		if not primaryKey:
+			#如果遍历完后没有主键，抛出错误
 			raise RuntimeError('Primary key not found')
+
+		#把attrs里除了主键意外的其他键值去掉
 		for k in mappings.keys():
 			attrs.pop(k)
 
-		escaped_fields = list(map(lambda f : '`%s`' % f, fields))
-		attrs['__mappings__'] = mappings  #保存属性和列的映射关系
-		attrs['__table__'] = tableName
-		attrs['__primary_key__'] = primaryKey #主键属性名
-		attrs['__fields__'] = fields # 除主键外的属性名
+
+		escaped_fields = list(map(lambda f : '`%s`' % f, fields))	#把fields的值全部加了个 ``
+		attrs['__mappings__'] = mappings  		#保存属性和列的映射关系
+		attrs['__table__'] = tableName			#表名
+		attrs['__primary_key__'] = primaryKey 	#主键属性名
+		attrs['__fields__'] = fields 			# 除主键外的属性名
 		# 构造默认的SELECT, INSERT, UPDATE和DELETE语句:
 		attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
 		attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
 		attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
 		attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+		#构造类
 		return type.__new__(cls, name, bases, attrs)
 
-		
+#Model类，元类是ModelMetalclass
 class Model(dict, metaclass = ModelMetalclass):
 	def __init__(self, **kw):
 		super(Model, self).__init__(**kw)
 
+	#重写访问属性的方法，没有属性和key一样则抛错
 	def __getattr__(self, key):
 		try:
 			return self[key]
 		except KeyError:
 			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
-
+	#重写写属性的方法
 	def __setattr__(self, key, value):
 		self[key] = value
 		
 	def getValue(self, key):
 		return getattr(self, key, None)
 
+	#访问某个key的方法，如果value是None，则去mappings获取default值
 	def getValueOrDefault(self, key):
 		value = getattr(self, key, None)
 		if value is None:
@@ -189,13 +195,14 @@ class Model(dict, metaclass = ModelMetalclass):
 
 	@classmethod
 	@asyncio.coroutine
+	#根据主键查找pk的值，取第一条
 	def find(cls, pk):
 		'find object by primary key'
-		rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+		rs = yield from select('%s where `%s`= ?' % (cls.__select__, cls.__primary_key__), [pk], 1)
 		if len(rs) == 0:
 			return None
 		return cls(**rs[0])
-
+	#根据当前类的属性，往相关table里插入一条数据
 	@asyncio.coroutine
 	def save(self):
 		args = list(map(self.getValueOrDefault, self.__fields__))
@@ -204,7 +211,7 @@ class Model(dict, metaclass = ModelMetalclass):
 		rows  = yield from execute(self.__insert__, args)
 		if rows != 1:
 			logging.warn('failed to insert record: affected rows :%s' % rows)
-
+	#跟新条目数据
 	@asyncio.coroutine
 	def update(self):
 		args = list(map(self.getValue, self.__fields__))
@@ -212,7 +219,7 @@ class Model(dict, metaclass = ModelMetalclass):
 		rows = yield from execute(self.__update__, args)
 		if rows != 1:
 			logging.warn('failed to update by primary key: affected rows: %s' % rows)
-
+	#根据主键的值删除条目
 	@asyncio.coroutine
 	def remove(self):
 		args = [self.getValue(self.__primary_key__)]
