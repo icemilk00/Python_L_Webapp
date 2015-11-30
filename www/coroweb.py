9b5f62fe445350718b8dc8c617650c63afa098e6
icemilk00 <442377973@qq.com>
2015-11-30 18:50:18 +0800

    add code

--- www/coroweb.py
+++ www/coroweb.py
@@ -1,3 +1,50 @@
+def get(path):
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kw):
+			return func(*args, **kw)
+		wrapper.__method__ = 'GET'
+		wrapper.__route__ = path
+		return wrapper
+	return decorator
+
+def post(path):
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kw):
+			return func(*args, **kw)
+		wrapper.__method__ = 'POST'
+		wrapper.__route__ = path
+		return wrapper
+	return decorator
+
+class RequestHandler(object):
+	"""docstring for RequestHandler"""
+	def __init__(self, app, fn):
+		self._app = app
+		self._fn = fn
+		self._has_request_arg = has_request_arg(fn)
+		self._has_var_kw_arg = has_var_kw_arg(fn)
+		self._has_named_kw_arg = has_named_kw_arg(fn)
+		self._named_kw_args = get_named_kw_args(fn)
+		self._required_kw_args = get_required_kw_args(fn)
+
+	@asyncio.coroutine
+	def  __call__():
+		
+		
+
+def add_route(app, fn):
+	method = getattr(fn, '__method__', None)
+	path = getattr(fn, '__route__', None)
+	if path is None or method is None:
+		raise ValueError('@get or @post not defined in %s.' % str(fn))
+	if not asyncio.iscoroutine(fn) and not inspect.isgeneratorfunction(fn):
+		fn = asyncio.coroutine(fn)
+	logging.info('add route %s %s => %s (%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+	app.route.add_route(method, path, RequestHandler(app, fn))
+	
+
 def add_routes(app, module_name):
 	n = module_name.rfind('.')
 	if n == (-1)
---
+++ www/handers.py
@@ -0,0 +1,16 @@
+import re, time, json, logging, hashlib, base64, asyncio
+
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
+logging.basicConfig(level=logging.DEBUG)
+
+@get('/')
+def index(request):
+	users = yield from User.findAll()
+	logging.info('to index...')
+	return {
+		'__template__':'test.html'
+		'users': users
+	}
