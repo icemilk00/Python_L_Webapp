cd228005411a1350f4822a0b52aece23e62b97a2
icemilk00 <442377973@qq.com>
2015-12-02 01:08:25 +0800

    code for coroweb.py

--- www/app.py
+++ www/app.py
@@ -9,6 +9,27 @@ from config import configs
 
 from coroweb import add_routes, add_static
 
+def init_jinja2(app, **kw):
+    logging.info('init jinja2...')
+    options = dict(
+        autoescape = kw.get('autoescape', True),
+        block_start_string = kw.get('block_start_string', '{%'),
+        block_end_string = kw.get('block_end_string', '%}'),
+        variable_start_string = kw.get('variable_start_string', '{{'),
+        variable_end_string = kw.get('variable_end_string', '}}'),
+        auto_reload = kw.get('auto_reload', True)
+    )
+    path = kw.get('path', None)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    logging.info('set jinja2 template path: %s' % path)
+    env = Environment(loader=FileSystemLoader(path), **options)
+    filters = kw.get('filters', None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    app['__templating__'] = env
+
 @asyncio.coroutine
 def logger_factory(app, handler):
 	@asyncio.coroutine
@@ -18,7 +39,7 @@ def logger_factory(app, handler):
 	return logger
 
 @asyncio.coroutine
-def response_factory():
+def data_factory(app, handler):
 	@asyncio.coroutine
 	def parse_data(request):
 		if request.method == 'POST':
@@ -31,6 +52,46 @@ def response_factory():
 		return (yield from handler(request))
 	return parse_data
 	
+@asyncio.coroutine
+def response_factory(app, handler):
+    @asyncio.coroutine
+    def response(request):
+        logging.info('Response handler...')
+        r = yield from handler(request)
+        if isinstance(r, web.StreamResponse):
+            return r
+        if isinstance(r, bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        if isinstance(r, str):
+            if r.startswith('redirect:'):
+                return web.HTTPFound(r[9:])
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+        if isinstance(r, dict):
+            template = r.get('__template__')
+            if template is None:
+                resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;charset=utf-8'
+                return resp
+        if isinstance(r, int) and t >= 100 and t < 600:
+            return web.Response(t)
+        if isinstance(r, tuple) and len(r) == 2:
+            t, m = r
+            if isinstance(t, int) and t >= 100 and t < 600:
+                return web.Response(t, str(m))
+        # default:
+        resp = web.Response(body=str(r).encode('utf-8'))
+        resp.content_type = 'text/plain;charset=utf-8'
+        return resp
+    return response
+
 def datetime_filter(t):
     delta = int(time.time() - t)
     if delta < 60:
@@ -55,6 +116,10 @@ def init(loop):
 	])
 	init_jinja2(app, filter=dict(datetime=datetime_filter))
 	add_routes(app, 'handers')
+	add_static(app)
+	srv = yield from loop.create_server(app.make_handle, '127.0.0.1', 9000)
+	logging.info('server started at http://127.0.0.1:9000...')
+    return srv
 
 
 
--- www/coroweb.py
+++ www/coroweb.py
@@ -1,3 +1,11 @@
+import asyncio, os, inspect, logging, functools
+
+from urllib import parse
+
+from aiohttp import web
+
+from apis import APIError
+
 def get(path):
 	def decorator(func):
 		@functools.wraps(func)
@@ -84,9 +92,59 @@ class RequestHandler(object):
 	def  __call__(self, request):
 		kw = None
 		if self._has_var_kw_arg or self._has_named_kw_arg or self._required_kw_args:
+			if request.method == 'POST':
+				if not request.content_type:
+					return web.HTTPBadRequest('Missing Content-Type')
+				ct = request.content_type.lower()
+				if ct.startswith('application/json'):
+					params = yield from request.json()
+					if not isinstance(params, dict):
+						return web.HTTPBadRequest('JSON Body must be object')
+					kw = params
+				elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data')
+					params = yield from request.post()
+					ks = dict(**params)
+				else:
+					return web.HTTPBadRequest('Unsupported Content-Type:%s' % request.content_type)
+			if request.method == 'GET':
+				qs = request.query_string
+				if qs:
+					kw = dict()
+					for k, v in parse.parse_qs(qs, True).items():
+						kw[k] = v[0]
+		if kw is None:
+			kw = dict(**request.match_info)
+		else:
+			if not self._has_var_kw_arg and self._named_kw_args:
+
+				copy =  dict()
+				for name in self._named_kw_args:
+					if name in kw:
+						copy[name] = kw[name]
+				kw = copy
+
+			for k, v in request.match_info.items():
+				if k in kw:
+					logging.warning('Duplicate arg name in named arg and kw args: %s' % k)	
+				kw[k] = v
+		if self._has_request_arg:
+			kw['request'] = request
 			
-		
-		
+		if self._required_kw_args:
+			for name in self._required_kw_args:
+				if not name in kw:
+					return web.HTTPBadRequest('Missing argument: %s' % name)
+		 logging.info('call with args: %s' % str(kw))
+		 try:
+		 	r = yield from self._func(**kw)
+		 	return r
+		 except APIError as e:
+		 	return dict(error=e.error, data=e.data, message=e.message)
+
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static %s => %s' % ('/static/', path))
 
 def add_route(app, fn):
 	method = getattr(fn, '__method__', None)
@@ -98,7 +156,6 @@ def add_route(app, fn):
 	logging.info('add route %s %s => %s (%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
 	app.route.add_route(method, path, RequestHandler(app, fn))
 	
-
 def add_routes(app, module_name):
 	n = module_name.rfind('.')
 	if n == (-1)
